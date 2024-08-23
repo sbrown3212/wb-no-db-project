fe948aacf2c3615f9e998d75389a94d59ca43d04
Stephen Brown <stephen.jr36@gmail.com>
2024-08-23 12:26:37 -0600

    added api call and app component  passes item data as a prop to 'WhislistTable' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-
+import { useState, useEffect } from 'react'
 import './App.css'
 import WishlistTable from './components/WishlistTable'
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  // Establish state for itemData as empty array
+  const [itemData, setItemData] = useState([]);
+
+  // Get itemData from server only once and set response data to itemData
+  useEffect(() => {
+    axios.get('/api/items')
+    .then((response) => {
+      // console.log(`RESPONSE DATA:`, response.data)
+      setItemData(response.data.items);
+    })
+  }, [])
 
   return (
     <>
-      <WishlistTable />
+      <WishlistTable initialData={itemData}/>
     </>
   )
 }
